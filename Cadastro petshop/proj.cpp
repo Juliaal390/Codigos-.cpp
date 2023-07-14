1ed081ae605e53a3b9816ea08d665ff842e219db
Juliaal390 <juliaa.brito22@gmail.com>
2023-07-14 11:52:35 -0300

    commit 3
    Cadastro completo

--- Cadastro petshop/proj.cpp
+++ Cadastro petshop/proj.cpp
@@ -1,7 +1,7 @@
 /**Programa que cadastra animais para banho em um petshop.
-Dados: esp?cie, ra?a, nome, dono, porte, contato.
-Crie um menu de op??es para:
-	- Cadastro: menu com as opções - cliente: nome, telefone.   -animal: dono, espécie, raça, nome, porte, contato; - voltar
+Dados: especie, raça, nome, dono, porte, contato.
+Crie um menu de opções para:
+	- Cadastro: menu com as opções - cliente: nome, telefone.   -animal: dono, esp?cie, ra?a, nome, porte, contato; - voltar
 	- Buscar dados;
 	- Alterar dados;
 	- Listagem de clientes (em ordem crescente de n?mero de telefone)
@@ -17,8 +17,7 @@ int main (){
     //variáveis
     char resposta, resposta_2;
     int opcao, indice=1, opcao_2, ind=0; //se indices não tiverem valor declarado, pode sobrar "lixo"
-    char resp = toupper(resposta_2); //cria a var resp, que = resposta_2 (em maiúscula)
-    string nome_cliente[100], telefone[100], nome_animal[100], especie[100], raca[100], porte[100], dono;
+    string nome_cliente[100], telefone[100], nome_animal[100], especie[100], raca[100], porte[100], dono, dono_animal[100];
     //
     do{
 
@@ -52,18 +51,22 @@ int main (){
 
     switch(opcao){
         case 1:{
+
+            do{
             cout<<"====== MENU DE CADASTRO ======"<<endl;
             cout<<"(A) Cliente"<<endl;
             cout<<"(B) Animal"<<endl;
             cout<<"(C) Voltar ao menu principal"<<endl;
             cin>>resposta_2;
 
-            if(resp=='A'){
-                opcao_2==1;
-                
+            if(toupper(resposta_2)=='A'){
+                opcao_2=1;   
             }
-            else if(resp=='B'){
+            else if(toupper(resposta_2)=='B'){
                opcao_2=2;
+            }
+            else if(toupper(resposta_2)=='C'){
+               opcao_2=3;
             }
 
             switch (opcao_2)
@@ -74,7 +77,7 @@ int main (){
                 getline(cin, nome_cliente[indice]); //getline para strings compostas
                 cout<<"Telefone: ";
                 getline(cin, telefone[indice]);
-                cout<<endl;
+                cout<<endl<<"Cadastro realizado com sucesso"<<endl;
                 indice++;
                 break;
             }
@@ -92,20 +95,20 @@ int main (){
                 getline(cin, dono);
 
                 for(int i=0; i<indice; i++){
-                    if(dono==nome_cliente[i]){
-                        
+                    if (dono==nome_cliente[i]){
+                        dono_animal[ind]=nome_cliente[i];
                     }
                 }
-            }
-
-            cout<<"Cadastro Realizado"<<endl;
-            break;
-
-            }
-            }
+                cout<<endl<<"Cadastro realizado com sucesso"<<endl;
+                ind++;
+                break;  } //case 2
+            } //switch dentro do switch
+        
         case 2:{
 
         }
+            }while(opcao_2!=3); 
+} //switch principal
  
     }
 } while(opcao!=7); //enquanto opcao não for = 7 (sair), o programa continuará em looping
